a106207ab8c708414d54ab11addfb7d39e63505e
Marat Gumerov <gumerovm74@gmail.com>
2021-08-02 15:08:10 +0300

    refactoring: reload page

--- task02_conversion_types/src/calculator.js
+++ task02_conversion_types/src/calculator.js
@@ -10,6 +10,15 @@ export class Calculator {
     this._SimpleComparison = '==';
     this._StrictComparison = '===';
     this._ifOperator = 'if';
+    this.action = 2;
+
+    this._panelBlock = document.querySelector('.operands__wrapper');
+    this._panel = document.querySelector('.first-operand');
+    this._btnReloadWrapper = document.querySelector('.btnReload-wrapper');
+  }
+
+  restoringPage () {
+    document.location.reload();
   }
 
   setOperator(str) {
@@ -22,60 +31,82 @@ export class Calculator {
   }
 
   hide (str) {
-    document.querySelector(str)
-      .className += " hide";
+     document.querySelector(str).className += ` hide`;
   }
 
-  //---------------Another thing that I'd like to mention - I can't repeat comparisons.
-  //--------------- I have to reload the page after the first try.
+  // show (str) {
+  //   const el = document.querySelector(str);
+  //   (/hide$/.test(el.className))?
+  //     el.className = el.className.slice(0, -5):
+  //     console.log(`ошибка: нету совпадение в элементе с классом ${el.className}`);
+  // }
+
   calculate() {
     let conversion;
+
     if (this.operator === this._ifOperator) {
       this.onDisplay(`${this.memory}   --- is be  ${this.result}`)
+      this.addreoladBtn();
     } else {
       const first = this.result[0][1];
       const second = this.result[1][1];
-      if (this.operator === this._SimpleComparison) {
-        conversion = first == second;
-      } else if (this.operator === this._StrictComparison) {
+
+      (this.operator === this._SimpleComparison)?
+        conversion = first == second:
         conversion = first === second;
-      }
+
       this.onDisplay(`${this.memory}   --- is be  ${conversion}`);
-    }
+      this.addreoladBtn();
+      };
   }
 
-  //-------------It is better to split this logic into separate methods.
+  addreoladBtn() {
+    let btnReload = document.createElement('button');
+    btnReload.className = "btnReload";
+    btnReload.innerHTML = "<strong>reload</strong>";
+    this._btnReloadWrapper.appendChild(btnReload);
+    btnReload.addEventListener('click', () => {
+      this.restoringPage()});
+  }
+
+  deleteBtnReload() {
+    const btn = document.querySelector('.btnReload');
+    btn.parentNode.removeChild(btn);
+  }
+
   addOperand() {
     let action = 2;
     const wrapper = document.querySelector('.operands__wrapper');
     wrapper.addEventListener('click', event => {
       // сепарация по оператору
       // '==' && '==='
-        if (action > 0 && (this.operator === this._SimpleComparison || this.operator === this._StrictComparison)) {
-          if (event.target.className === 'operand__item') {
-            if (this.memory && action) {
-              this.result.push([`${event.target.innerHTML}`, this.valueBase[event.target.dataset.item]]);
-              this.memory += `${this.operator} ${event.target.innerHTML}`;
-              this.onDisplay(`${this.memory}`);
-              action--;
-            } else if (!this.memory && action) {
-              this.result =[[`${event.target.innerHTML}` , this.valueBase[event.target.dataset.item]]];
-              this.memory = event.target.innerHTML;
-              this.onDisplay(this.memory);
-              action--;
-            }
-          }
-      // 'if'
-        } else if (action > 0 && (this.operator === this._ifOperator)) {
-          if (event.target.className === 'operand__item') {
-            if (!this.memory) {
-              this.memory = `${this.operator} (${event.target.innerText})`;
-              this.result = Boolean(this.valueBase[event.target.dataset.item]);
-              this.onDisplay(this.memory);
-              action = 0;
-            }
+      if (action > 0 && (this.operator === this._SimpleComparison || this.operator === this._StrictComparison)) {
+        if (event.target.className === 'operand__item') {
+          if (this.memory && action) {
+            this.result.push([`${event.target.innerHTML}`, this.valueBase[event.target.dataset.item]]);
+            this.memory += `${this.operator} ${event.target.innerHTML}`;
+            this.onDisplay(`${this.memory}`);
+            action--;
+          } else if (!this.memory && action) {
+            this.result = [
+              [`${event.target.innerHTML}`, this.valueBase[event.target.dataset.item]]
+            ];
+            this.memory = event.target.innerHTML;
+            this.onDisplay(this.memory);
+            action--;
           }
         }
+        // 'if'
+      } else if (action > 0 && (this.operator === this._ifOperator)) {
+        if (event.target.className === 'operand__item') {
+          if (!this.memory) {
+            this.memory = `${this.operator} (${event.target.innerText})`;
+            this.result = Boolean(this.valueBase[event.target.dataset.item]);
+            this.onDisplay(this.memory);
+            action = 0;
+          }
+        }
+      }
       if (!action) {
         this.hide('.first-operand');
         this.calculate();
