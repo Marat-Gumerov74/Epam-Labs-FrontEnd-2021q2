61fe55d56d35d279d7789580d36dbdb19d5dba8f
Marat Gumerov <gumerovm74@gmail.com>
2021-08-03 18:29:58 +0300

    done: func add

--- task03/scr/index.js
+++ task03/scr/index.js
@@ -1,1 +1,22 @@
-console.log(`test`);
+'use strict';
+
+const a = {x: 1};
+const b = {x: 2, y: 2, t: 5};
+const resObj = add(a, b, a);
+
+console.log('result obj:', resObj);
+
+
+ function add(...collectObj) {
+  let result = Object.assign({}, collectObj[0]);
+
+  for (let i = 1; i < collectObj.length; i++) {
+    for (let key in collectObj[i]) {
+      (result.hasOwnProperty(key)) ?
+      result[key] = result[key] + collectObj[i][key]:
+      result[key] = collectObj[i][key];
+    };
+  };
+
+  return result;
+};
