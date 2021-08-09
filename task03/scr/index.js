d64d8da07d51824ddac0e3a1ddbb797f50c34dc7
Marat Gumerov <gumerovm74@gmail.com>
2021-08-09 12:33:32 +0300

    refactoring: add intersept method

--- task03/scr/index.js
+++ task03/scr/index.js
@@ -3,8 +3,10 @@
 const a = {x: 1};
 const b = {x: 2, y: 2, t: 5};
 const resObj = add(a, b, a);
+const resInter = intersect(a,b);
 
 console.log('result obj:', resObj);
+console.log('result resInter:', resInter);
 
 
  function add(...collectObj) {
@@ -17,6 +19,19 @@ console.log('result obj:', resObj);
       result[key] = collectObj[i][key];
     };
   };
+  return result;
+};
+
+  function intersect(...collectObj) {
+    let result = Object.assign({}, collectObj[0]);
+
+    for (let i = 1; i < collectObj.length; i++) {
+      for (let key in collectObj[i]) {
+        (result.hasOwnProperty(key)) ?
+        result["new" + key] = collectObj[i][key]:
+          result[key] = collectObj[i][key];
+      };
+    };
 
   return result;
 };
