7924605ae28b4264377915e832adaa2b9d9953b9
Marat Gumerov <gumerovm74@gmail.com>
2021-08-09 14:40:32 +0300

    refactor:  refactoring intersect()

--- task03/scr/index.js
+++ task03/scr/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
-const a = {x: 1};
+const a = {x: 1, t:5};
 const b = {x: 2, y: 2, t: 5};
+const c = {x: 4, t: 1};
+
 const resObj = add(a, b, a);
-const resInter = intersect(a,b);
+const resInter = intersect(a,b,c);
 
 console.log('result obj:', resObj);
 console.log('result resInter:', resInter);
@@ -19,20 +21,23 @@ console.log('result resInter:', resInter);
       result[key] = collectObj[i][key];
     };
   };
-  
+
   return result;
 };
 
   function intersect(...collectObj) {
-    let result = Object.assign({}, collectObj[0]);
+    let copyfirst = Object.assign({}, collectObj[0]);
+    let result = Object.create(null);
 
-    for (let i = 1; i < collectObj.length; i++) {
+    for (let i = 0; i < collectObj.length; i++) {
       for (let key in collectObj[i]) {
-        if (result.hasOwnProperty(key)) {
-           result["new" + key] = collectObj[i][key];
-        };
-      };
-    };
+        if (copyfirst.hasOwnProperty(key)) {
+          result[i + key] = collectObj[i][key];
+        } else {
+          delete copyfirst[key];
+        }
+      }
+    }
 
   return result;
 };
