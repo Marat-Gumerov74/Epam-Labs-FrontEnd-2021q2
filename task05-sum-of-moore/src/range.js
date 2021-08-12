47dd7bb33ae7074897db982bdca55ed6a038753d
Marat Gumerov <gumerovm74@gmail.com>
2021-08-12 23:25:50 +0300

    add: add simple solution in index js

--- task05-sum-of-moore/src/range.js
+++ task05-sum-of-moore/src/range.js
@@ -1,37 +1,35 @@
-const a = 10;
-const b = 900000000000;
-const result2 = range(a, b);
-
-console.log(`result function range: ${result2}`);
-
-
 function range(min, max) {
 
   function makeBigSum(a) {
     return function (b) {
-      return BigInt ((a + b) * ((b - a + 1) / 2));
-    };
+      return (BigInt(a) + BigInt(b)) * ((BigInt(b) - BigInt(a) + 1) / 2);
+    }
   }
 
   function makeNormalSum(a) {
     return function (b) {
       return (a + b) * ((b - a + 1) / 2);
-    };
+    }
   }
 
   try {
     let result = null;
 
     if (typeof min != 'number' || typeof max != 'number') {
-      throw new SyntaxError("Вы ввели не число!");
+      throw new SyntaxError("Вы ввели не число!")
     }
 
     result = (makeNormalSum(min)(max) > Number.MAX_SAFE_INTEGER) ?
-      makeBigSum(min)(max) :
-      makeNormalSum(min)(max);
-    
-    return result;
-  } catch (e) {
+     makeBigSum(min)(max):
+     makeNormalSum(min)(max);
+
+    }
+
+   catch(e) {
     alert('Извините в данных ошибка')
   }
+
+  return result;
 }
+
+module.exports = range;
