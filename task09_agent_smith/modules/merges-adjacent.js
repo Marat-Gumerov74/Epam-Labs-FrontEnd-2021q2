d9cfbaea4824b110c0f8a85fb670ab698f1b3b0c
Marat Gumerov <gumerovm74@gmail.com>
2021-09-06 22:09:52 +0300

    feat: add modules

---
+++ task09_agent_smith/modules/merges-adjacent.js
@@ -0,0 +1,11 @@
+function mergesAdjacent (arr) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        let left = (arr[i-1])? arr[i-1]: 0;
+        let right = (arr[i+1])? arr[i+1]:0;
+        newArr[i] = left + right;
+    }
+    return newArr;
+}
+
+module.exports = { mergesAdjacent }
---
+++ task09_agent_smith/src/index.js
@@ -0,0 +1,6 @@
+const { mergesAdjacent } = require('../modules/merges-adjacent');
+
+let arr = [1, 2, 3, 4];
+
+let resultArr = mergesAdjacent(arr);
+console.log(resultArr);
