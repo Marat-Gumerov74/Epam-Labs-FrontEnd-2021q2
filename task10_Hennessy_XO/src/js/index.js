a50bc7ec1e22217e4772ed33a3bb62c17bc577b2
Marat Gumerov <gumerovm74@gmail.com>
2021-09-17 14:37:05 +0300

    add: block after win

--- task10_Hennessy_XO/src/js/index.js
+++ task10_Hennessy_XO/src/js/index.js
@@ -8,9 +8,12 @@ window.onload = function () {
     renderField();
     info.startInfo();
     let step = 0;
+    let resultGame = checkGame(step);
+    console.log(resultGame)
 
     document.getElementById('game').onclick = function (event) {
-        if (event.target.className === 'block') {
+        resultGame = checkGame(step);
+        if (event.target.className === 'block' && !resultGame) {
             if(step % 2 === 0) {
                 event.target.innerHTML = 'X';
             } else {
@@ -18,14 +21,12 @@ window.onload = function () {
             }
             step++;
             let resultGame = checkGame(step);
-            console.log(`result game`, resultGame)
+            console.log(`result game`, resultGame);
             if (resultGame) {
                 info.announcedWinner(resultGame);
             }
-
         }
     }
-
 }
 
 
