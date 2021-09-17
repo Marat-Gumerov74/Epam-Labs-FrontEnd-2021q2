bd835ace7ca24867e5dc79d7baa0c97bf8c988bc
Marat Gumerov <gumerovm74@gmail.com>
2021-09-17 23:12:12 +0300

    stable version 1.0

--- task10_Hennessy_XO/src/js/index.js
+++ task10_Hennessy_XO/src/js/index.js
@@ -2,16 +2,14 @@ import { Info } from "./info.js";
 import { renderField } from "./renderField.js";
 import { checkGame } from "./checkGame.js";
 
-
 const info = new Info();
 window.onload = function () {
     renderField();
     info.startInfo();
     let step = 0;
     let resultGame = checkGame(step);
-    // Load score
 
-    let go = function (event) {
+    let onStep = function (event) {
         resultGame = checkGame(step);
         if (event.target.className === 'block' && !resultGame && event.target.innerHTML === "") {
             if(step % 2 === 0) {
@@ -22,14 +20,14 @@ window.onload = function () {
             step++;
             resultGame = checkGame(step);
             if (resultGame) {
-                document.getElementById('game').removeEventListener('click', go)
+                document.getElementById('game').removeEventListener('click', onStep)
                 info.announcedWinner(resultGame);
             }
         }
     }
 
     document.getElementById('game')
-        .addEventListener('click',  go );
+        .addEventListener('click',  onStep );
 }
 
 
