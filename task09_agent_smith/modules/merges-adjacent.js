c9bfe15ab6556db29751b7a8b29eadd1248ba55a
Marat Gumerov <gumerovm74@gmail.com>
2021-10-01 14:35:18 +0300

    refactoring: merges-adjacent (task 09)

--- task09_agent_smith/modules/merges-adjacent.js
+++ task09_agent_smith/modules/merges-adjacent.js
@@ -1,8 +1,8 @@
 function mergesAdjacent (arr) {
     let newArr = [];
     for (let i = 0; i < arr.length; i++) {
-        let left = (arr[i-1])? arr[i-1]: 0;
-        let right = (arr[i+1])? arr[i+1]:0;
+        let left =(arr[i-1])? arr[i-1]: 0;
+        let right =(arr[i+1])? arr[i+1]:0;
         newArr[i] = left + right;
     }
     return newArr;
